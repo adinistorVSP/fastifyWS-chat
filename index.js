8abc270fafb1962645f589d89005d8f3b3a7f955
Adi Nistor VSP <adrian.nistor@vspartners.us>
2022-06-27 11:01:27 +0300

    ws chat app

---
+++ index.js
@@ -0,0 +1,52 @@
+const fastify = require('fastify')();
+const path = require('path')
+
+fastify.register(require('@fastify/websocket'));
+fastify.register(require('@fastify/static'), {
+    root: path.join(__dirname, 'frontend')
+})
+
+fastify.addHook('preValidation', async (request, reply) => {
+    if(request.routerPath == '/chat' && !request.query.username) {
+        reply.code(403).send('Connection rejected');
+    }
+})
+
+fastify.get('/chat', { websocket: true }, (connection, req) => {
+    // New user
+    broadcast({
+        sender: '__server',
+        message: `${req.query.username} joined`
+    });
+
+    // Leaving user
+    connection.socket.on('close', () => {
+        broadcast({
+            sender: '__server',
+            message: `${req.query.username} left`
+        });
+    });
+
+    // Broadcast incoming message
+    connection.socket.on('message', (message) => {
+        message = JSON.parse(message.toString());
+        broadcast({
+            sender: req.query.username,
+            ...message
+        });
+    });
+});
+
+fastify.listen({ port: 3000 }, (err, address) => {
+    if(err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server listening at: ${address}`);
+});
+
+function broadcast(message) {
+    for(let client of fastify.websocketServer.clients) {
+        client.send(JSON.stringify(message));
+    }
+}
